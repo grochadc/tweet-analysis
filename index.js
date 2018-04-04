81c5f2650abfff549ecb714b78f9bb47aaa1e076
Gonzalo Rocha <grochadc@gmail.com>
2018-04-03 22:31:19 -0500

    Keep only the search function in this file

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
-/* Make a request to get a Bearer token from Twitter API */
+/*jshint esversion: 6 */
+
 var request = require('request');
+const token = process.env.BEARER_TOKEN;
 
-var consumer_key= process.env.CONSUMER_KEY;
-var consumer_secret= process.env.CONSUMER_SECRET;
+const reqAPI = new Promise((resolve, reject) =>{
+    request.get({
+      headers: {
+        'Authorization' : 'Bearer ' + token,
+      },
+      url : 'https://api.twitter.com/1.1/search/tweets.json?q=%23yomero'
+    }, (err, response, body) => {
+      if (err) reject(err);
+      var data = {response, body};
+      resolve(data);
+    });
+  });
 
-var encoded = new Buffer(consumer_key+':'+consumer_secret).toString('base64');
-
-request.post({
-  headers: {'Content-Type' : 'application/x-www-form-urlencoded',
-            'Authorization' : 'Basic '+ encoded},
-  url:     'https://api.twitter.com/oauth2/token',
-  body:    "grant_type=client_credentials"
-}, function(error, response, body){
-  console.log(JSON.parse(body).access_token);
+reqAPI.then((data) => {
+  var tweets = JSON.parse(data.body).statuses;
+  console.log('API returned ',tweets.length, ' tweets');
+  for(var i in tweets) {
+    var tweet = tweets[i];
+    var user = tweet.user;
+    console.log('Tweet number ',i, ':');
+    console.log('Username: ', user.screen_name);
+    console.log('Verified: ', user.verified);
+    console.log('Followers: ', user.followers_count);
+    console.log('Created: ', user.created_at);
+  }
 });
